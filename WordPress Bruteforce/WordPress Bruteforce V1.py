ab2c095b443085438f770c566807fb9e51b237c9
Christian Schwarz <christian.schwarz92@gmx.de>
2020-07-22 18:55:16 +0200

    Update WordPress Bruteforce V1.py

--- WordPress Bruteforce/WordPress Bruteforce V1.py
+++ WordPress Bruteforce/WordPress Bruteforce V1.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path = r"D:\Downloads\chromedriver_win32\chromedriver.exe")
-driver.get('http://[yourfancydomainname]wp-login')
+driver.get('http://[yourfancydomainname]/wp-login')
 
 user_input = driver.find_element_by_id("user_login")
 user_input.send_keys('admin')
